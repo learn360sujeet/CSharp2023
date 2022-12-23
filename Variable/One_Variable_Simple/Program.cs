b47f07898e84915b50e4b17ac1b49c30b679eba6
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-23 19:53:19 +0530

    Variable _Simple _Coding

--- Variable/One_Variable_Simple/Program.cs
+++ Variable/One_Variable_Simple/Program.cs
@@ -1,1 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+int num;//Variable Declaration 
+num = 200;//storing the value to variable
+Console.WriteLine("This is your number {0}", num);//Print the Value stored in Container like Variable
+Console.ReadLine();// Command to hold the console Window 
