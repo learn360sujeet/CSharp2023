fcd5c79f82b16a8bd74326f63489ccb37b5d9f94
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-23 20:30:28 +0530

    Boxing and Unboxing

--- Variable/Four_Variable_Boxing_Unboxing/Program.cs
+++ Variable/Four_Variable_Boxing_Unboxing/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bhola Prasad");
+///Boxing is the process of converting value type to refrence type
+
+int numberOne = 100;
+object obj = numberOne;
+Console.WriteLine(obj);// Boxing
+
+///Unboxing is the process of converting refrence type to value type
+ 
+
+int numberTwo = (int)obj;//Every Conversion results a value ,that has to be contained in a container like type and variable
+Console.WriteLine(numberTwo);
