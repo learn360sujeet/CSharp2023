a6d185b2b2b2cd8d3819e7a02041e30f2ef23464
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-23 20:38:55 +0530

    UserInput

---
+++ Variable/Six_Variable_UserInput/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your Fisrt Name");
+Console.Write("Name has to be in Capital Letter Only:");
+string userName=Console.ReadLine();
+Console.WriteLine("Enter your Last Name");
+Console.Write('"');
+string userNameSecond = Console.ReadLine();
+Console.WriteLine("Hello {0} {1}",userName,userNameSecond);
+Console.ReadLine();
