ef48d5ecfa6a926f09f97b9a5b197bc36f7ab109
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-23 19:58:54 +0530

    Constant Variable Functions

--- Variable/Two_Variable_Constant/Program.cs
+++ Variable/Two_Variable_Constant/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+const int firstNumber = 200;
+
+Console.WriteLine("Number stored in Const is :{0}", firstNumber);
