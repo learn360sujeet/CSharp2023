e60a3b8eb6ac83eb6dfa71c74959b0917d4dc1ab
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-23 20:15:13 +0530

    TypeCasting .

--- Variable/Three_Variable_TypeCasting/Program.cs
+++ Variable/Three_Variable_TypeCasting/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+byte smallNumber = 100;
+int largeNumber = (int)smallNumber;
+Console.WriteLine(largeNumber);//Implicit Casting
+double largerNumber = 300.45;
+int smallerNumber = (int)largerNumber;//Explicit Casting //Type Casting//Data may be lost 
+Console.WriteLine(smallerNumber);
