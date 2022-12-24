5f455465cb65885321885621fdf69f288b0c8b6c
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2022-12-24 16:10:06 -0600

    Interview Question - Varibale
    Interview Question - Varibale

--- Variable/Three_Variable_TypeCasting/Program.cs
+++ Variable/Three_Variable_TypeCasting/Program.cs
@@ -1,8 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
 byte smallNumber = 100;
 int largeNumber = smallNumber;
+
 Console.WriteLine(largeNumber);//Implicit Casting
+
 double largerNumber = 300.45;
 int smallerNumber = (int)largerNumber;//Explicit Casting //Type Casting//Data may be lost 
+
 Console.WriteLine(smallerNumber);
