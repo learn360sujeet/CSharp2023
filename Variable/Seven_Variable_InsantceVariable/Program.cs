9c8bd521731a68cce2eecf92836f7d7a48028eca
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-24 06:41:30 +0530

    Instance Variable

---
+++ Variable/Seven_Variable_InsantceVariable/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class InsatnceVariable
+{
+    int c;
+    public  void Add()
+    {
+        int a = 100;
+        int b = 200;
+        int c =a+b;
+        Console.WriteLine(c);
+
+    }
+
+    public  void Sub()
+    {
+        int a = 200;
+        int b = 100;
+        int c = b - a;
+        Console.WriteLine(c);
+    }
+
+    static void Main(string[] args)
+    {
+        InsatnceVariable obj= new InsatnceVariable();
+        obj.Add();
+        Console.ReadLine();
+    }
+   
+}
