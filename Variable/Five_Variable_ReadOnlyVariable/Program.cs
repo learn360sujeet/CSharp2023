02e72a505ba156641a2ed6488386f11a56b32e30
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2022-12-24 20:14:57 -0600

    Read-only Variable code and interview Question

--- Variable/Five_Variable_ReadOnlyVariable/Program.cs
+++ Variable/Five_Variable_ReadOnlyVariable/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+#region Way 1 of using read only 
+
+using System;
+class Program_B
+{
+
+    //readonly int age = 18; // read-only variable
+    //public static void Main()
+    //{
+    //    Program_B classObject = new Program_B(); // object creation
+
+    //    Console.WriteLine("Value of age : " + classObject.age);
+
+    //    Console.Read();
+    //}
+}
+
+#endregion
+
+
+#region Way 2 of using read only variable 
+class Program_way2
+{
+
+    //// Optional to put value in read only variable 
+    //readonly int age;
+    //public static void Main()
+    //{
+    //    Program_way2 classObject = new Program_way2(); // object creation
+
+    //    Console.WriteLine("Value of age : " + classObject.age);
+
+    //    Console.Read();
+    //}
+}
+#endregion
+
+
+class Program_way3
+{
+
+    // Optional to put value in read only variable 
+    readonly int age;
+
+
+    // Constructor - A special method having same name as class 
+    Program_way3()
+    {
+        age = 18;
+
+    }
+
+    public static void Main()
+    {
+        Program_way3 classObject = new Program_way3(); // object creation
+
+        // To access Read only  variable , you do  need to create object of class 
+        Console.WriteLine("Value of age : " + classObject.age);
+
+        Console.Read();
+    }
+}
--- Variable/Two_Variable_Constant/Program.cs
+++ Variable/Two_Variable_Constant/Program.cs
@@ -1,5 +1,18 @@
-﻿//// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-const int firstNumber = 200;
+﻿
+using System;
 
-Console.WriteLine("Number stored in Const is :{0}", firstNumber);
+/// <summary>
+/// https://www.educba.com/variables-in-c-sharp/
+/// </summary>
+class Program_A
+{
+
+    // Constant Variable must not chnage once value is assigned at the time declaration
+    // Mandatory to put value in constant variable at the of declaration
+    const float maxValue = 75; 
+    public static void Main()
+    {       
+        // To access constanct variable , you do not need to create object of class 
+        Console.WriteLine("Value of max " + Program_A.maxValue);
+    }
+}
