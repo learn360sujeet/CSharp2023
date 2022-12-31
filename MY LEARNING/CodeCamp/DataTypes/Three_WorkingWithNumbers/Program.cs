165b204c3adfb14d907caa46f9edaab45254c44c
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 16:55:38 +0530

    numbers amid operators

---
+++ MY LEARNING/CodeCamp/DataTypes/Three_WorkingWithNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Numbers
+{
+    static void Main()
+    {
+        int a = 5; int b = 7;
+
+        Console.WriteLine(  a+ b );
+
+        Console.WriteLine( b/a );
+
+        Console.WriteLine( b%a );
+
+        Console.WriteLine( b-a );
+
+        Console.ReadLine();
+            
+                
+
+    }
+}
