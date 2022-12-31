da79f4c743e494c7ef0a75454729d489b6569c56
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 17:36:20 +0530

    Simple Calculator

---
+++ MY LEARNING/CodeCamp/CalculatorMaking/One_Simple Cal/Program.cs
@@ -0,0 +1,20 @@
+﻿class Calculator
+{
+    static void Main()
+    {
+        Console.Write("Enter the first Number :");
+
+        int firstNum= Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the Second Number :");
+
+        int SecondNum= Convert.ToInt32(Console.ReadLine());
+
+        int sum = firstNum + SecondNum;
+
+        Console.WriteLine( " The Result is {0} ", sum);
+
+        Console.ReadLine(); 
+
+    }
+}
