5fe40c4c21eaf5c496ab4a5f2e5cd38160fc92a7
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 22:41:54 +0530

    for loop

---
+++ MY LEARNING/CodeCamp/Loop/Three_Loop_ForLoop/Program.cs
@@ -0,0 +1,14 @@
+﻿class For_Loop
+{
+    static void Main(string[] args)
+    {
+        //alternatives for while loop 
+        for (int i=1;i<=100;i++)
+        {
+            Console.WriteLine(i);
+
+           
+        }    
+        
+    }
+}
