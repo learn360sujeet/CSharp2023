213365848dbd8ff37e0bcd8b73118043c51d4104
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 17:18:55 +0530

    User Input

---
+++ MY LEARNING/CodeCamp/Getting User Input/One_UserInput/Program.cs
@@ -0,0 +1,20 @@
+﻿// WRITELINE (PRINT AND MOVE TO NEXT LINE IN CONSOLE WINDOW .
+
+// WRITE (PRINT AND HOLD ON TO THE CONSOLE WINDOW UNTILL USER INPUT .
+
+class UserInput
+{
+    static void Main ()
+    {
+        Console.Write(" KINDLY ENTER YOUR NAME :");
+
+        string  name = Console.ReadLine ();
+
+        Console.WriteLine ("Hello {0}", name );
+
+
+        Console.ReadLine(); 
+
+
+    }
+}
