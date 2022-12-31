c5a0b92347493031493f75a4613ba1f3ccc989a3
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 13:41:17 +0530

    Variable

---
+++ MY LEARNING/CodeCamp/Variable/One_Variable_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Variable
+{
+    static void Main(string[] args)
+    {
+        string userName = "Bhola";
+
+        int userAge = 35;
+
+        Console.WriteLine(" Hi my name is " + userName);
+
+        userName = "Sujeet";
+
+        Console.WriteLine(" " + userName + " age is " + userAge);
+
+        Console.ReadLine();
+
+    }
+
+}
+
+           
+
