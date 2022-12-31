34aeffceff319f9c23909c301eac565e59ac154a
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 15:06:26 +0530

    data types .

---
+++ MY LEARNING/CodeCamp/DataTypes/One_Datatypes/Program.cs
@@ -0,0 +1,18 @@
+﻿class DataTypes
+{
+    static void Main()
+    {
+        string userName = "ASSUS";
+
+        int smallNumber = 100;
+
+        bool IsSunnyDay = false;
+
+        char myNameFirstLetter = 'B';
+
+        Console.WriteLine(  " Display all The Variables Value : {0},{1},{2},{3}",userName,smallNumber,IsSunnyDay,myNameFirstLetter);
+
+        Console.ReadLine();
+
+    }
+}
