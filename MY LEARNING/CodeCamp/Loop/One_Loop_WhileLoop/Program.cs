a95a7a6dd0526eb0115e946cc6ab1ff7287ca6f8
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 22:25:32 +0530

    Do while loop concepts

--- MY LEARNING/CodeCamp/Loop/One_Loop_WhileLoop/Program.cs
+++ MY LEARNING/CodeCamp/Loop/One_Loop_WhileLoop/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         int myNumber = 100;
-
+        // while condition must have to be true 
         while(myNumber <=200)
         {
             Console.WriteLine(myNumber);
---
+++ MY LEARNING/CodeCamp/Loop/Two_DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Do_While
+{
+    static void Main()
+    {
+        int num1 = 20;
+        do
+        {
+            Console.WriteLine(num1);
+            num1++;
+  // condition false but looping done once 
+        } while (num1 <= 10);
+
+        Console.ReadLine();
+
+    }
+}
