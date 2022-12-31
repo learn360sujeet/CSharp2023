491b80dc2be7653e197615b2a20b4c359d313b3b
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 22:06:38 +0530

    While loop concepts

---
+++ MY LEARNING/CodeCamp/Loop/One_Loop_WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿class Loop_one
+{
+    static void Main(string[] args)
+    {
+        int myNumber = 100;
+
+        while(myNumber <=200)
+        {
+            Console.WriteLine(myNumber);
+
+                myNumber++;
+
+        }
+
+        Console.ReadLine();
+    }
+}
