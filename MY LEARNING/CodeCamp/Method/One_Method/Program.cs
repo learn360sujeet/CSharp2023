b084c3f861adf6940cb9c7e5662b10e228480440
learn360Sujeet <learn360sujeet@gmail.com>
2022-12-31 18:40:09 +0530

    Method Concepts

---
+++ MY LEARNING/CodeCamp/Method/One_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿//Method is block of code where certain actions are meant to be performed .
+
+class MethodLearning
+{
+    //Main method 
+    static void Main(string[] args)
+    {
+        BholaPrasad();
+        Console.ReadLine(); 
+
+    }
+
+    //method of returning value 
+    static void BholaPrasad()
+    {
+        
+        Console.WriteLine("Hello bhole shankar ");
+    }
+}
