10a3bd38356c8d844cd63ca0995cbb19ddda2080
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-01 17:35:13 +0530

    Introduction !

---
+++ MY LEARNING/ANKUR_Training/Introduction/One_Introduction/Program.cs
@@ -0,0 +1,9 @@
+﻿class Introduction
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(  "Welcome to learning c sharp");
+
+        Console.ReadLine();
+    }
+}
--- MY LEARNING/CodeCamp/Patterns/One_Pattern_Design/Program.cs
+++ MY LEARNING/CodeCamp/Patterns/One_Pattern_Design/Program.cs
@@ -3,22 +3,20 @@
 class Pattern
 {
     static void Main(string[] args)
-    {  
-        //ROWS
-        for (int i = 1; i <= 7; i++)
+    {
+        for (int i =1; i<=7; i++)
         {
-            //COLOUMNS
+
             for (int j = 1; j <= 10; j++)
             {
                 Console.Write("*");
-            }
 
-            Console.WriteLine();
+
+            }
+            
+            Console.WriteLine (); 
         }
         Console.ReadLine();
-    }
+
+    } 
 }
-
-
-
-     
