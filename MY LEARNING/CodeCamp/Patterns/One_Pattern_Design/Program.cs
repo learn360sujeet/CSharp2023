eb9807754a15a0e155743d2780a048f82c792dd4
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-01 09:28:02 +0530

    First design pattern

---
+++ MY LEARNING/CodeCamp/Patterns/One_Pattern_Design/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Pattern
+{
+    static void Main(string[] args)
+    {  
+        //ROWS
+        for (int i = 1; i <= 7; i++)
+        {
+            //COLOUMNS
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
+
+
+
+     
