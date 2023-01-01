ecfca76025fe227d3f90148b8d32bdc3252bc493
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-01 07:28:24 +0530

    Constructor concepts

---
+++ MY LEARNING/CodeCamp/Constructor/One_Constructor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Constructor_Concepts
+{
+
+    //class memeber declaration of variables
+
+    string bookName; 
+
+    int bookNumber;
+
+    public Constructor_Concepts(string myNmae )
+    {
+        Console.WriteLine(myNmae);
+    }
+
+    static void Main(string[] args)
+
+    {
+        Constructor_Concepts obj = new Constructor_Concepts("BHOLA");
+
+        obj.bookName = "Programming language ";
+
+        obj.bookNumber = 1; ;
+
+        Console.WriteLine( obj.bookName);
+
+        Console.WriteLine(obj.bookNumber);  
+
+        Console.ReadLine();
+    }
+}
