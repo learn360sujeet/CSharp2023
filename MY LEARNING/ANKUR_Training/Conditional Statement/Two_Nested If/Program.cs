45ff1ef8d410ba6d4c76acc438ad81f0bdcf0be9
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-01 19:09:27 +0530

    nested if

---
+++ MY LEARNING/ANKUR_Training/Conditional Statement/Two_Nested If/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Nested_If
+{
+    static void Main()
+    {
+        Console.Write("Enter the Number of your choice :");
+
+        int userNumber = int.Parse(Console.ReadLine()); 
+
+        if(userNumber == 100 ) 
+        {
+            Console.WriteLine("Entered number is 100");
+        }
+
+        else if(userNumber==300)
+        {
+            Console.WriteLine("Entered number is 300");
+
+        }
+
+        else
+        {
+            Console.WriteLine("Number is Invalid :");
+
+        }
+        Console.ReadLine();
+    }
+}
