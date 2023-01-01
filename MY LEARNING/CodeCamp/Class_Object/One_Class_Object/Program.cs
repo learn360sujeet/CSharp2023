acc36d081cf78951c07501b3d5f7a2a163e244cc
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-01 07:14:45 +0530

    Class_Object

---
+++ MY LEARNING/CodeCamp/Class_Object/One_Class_Object/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class LaptopDetails
+
+{
+    string laptopName;
+
+    string laptopColour;
+
+    string laptopNumber;
+
+
+    static void Main(string[] args)
+    {
+        LaptopDetails obj = new LaptopDetails();
+
+        obj.laptopName = "Asus";
+
+        obj.laptopNumber = "1";
+
+        obj.laptopColour = "green";
+
+        Console.WriteLine(obj.laptopName);
+        Console.WriteLine(obj.laptopColour);
+        Console.WriteLine(obj.laptopNumber);
+
+        Console.ReadLine(); 
+
+    }
+}
