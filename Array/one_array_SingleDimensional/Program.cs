dd14c0d2ceb241ff171473e5c21ea0de06f4c1a8
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-01-01 21:03:45 -0600

    loop and array

---
+++ Array/one_array_SingleDimensional/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//=================================================
+
+// step 1 - Declaration 
+int[] numbers = new int[5];
+
+
+
+// Step 2 - Initialize 
+numbers[0] = 25;
+numbers[1] = 250;
+numbers[3] = 900;
+
+
+// step 3 get the first element
+int first = numbers[0];
+
+//======================================================================
+
+// modify the third element
+numbers[0] = 30;
+    
+  
---
+++ loop/one_loop_forloop/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// simple problem 
+// 10 times uta baiti 
+// loop - start ,condition- till what this loop will contiunue, end 
+
+for (int i = 1; i <= 10; i++)
+{
+
+    Console.WriteLine("Uta- baiti -" + i.ToString());
+
+}
+
+
+Console.Read();
