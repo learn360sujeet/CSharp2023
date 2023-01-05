36d50728a941b09bc44dab6cbbbe9a527970bd7d
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-01-04 20:25:20 -0600

    foreach

---
+++ loop/Five_loop_foreach/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+string[] boyName = new string[3];
+
+boyName[0] = "Bhola"; // i=0
+boyName[1] = "Sujeet"; // i=1
+boyName[2] = "Ajeet"; // i=2;
+
+foreach (string k in boyName)
+{
+
+    Console.Write(k);
+
+}
