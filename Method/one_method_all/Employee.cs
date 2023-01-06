52d0aac8111f58d21d28c8b68ed0a319204bd8b0
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-01-05 20:20:02 -0600

    class method

---
+++ Method/one_method_all/Employee.cs
@@ -0,0 +1,32 @@
+﻿
+// namespace given by framework to utilize it 
+using System;
+
+
+
+
+// blue - that is given by framework 
+public class Employee 
+{
+    // data + Action 
+    // Varibale + Method 
+
+    // Name , Mobile , email 
+
+    // instance/objected variable 
+    public string name ="sujeet" ;
+    public double mobile =789087;
+    public string email ="s@gmail.com";
+
+    // void = return type 
+    // () - method symble
+    public void Display()
+    {
+        Console.WriteLine(name);
+        Console.WriteLine(mobile);
+        Console.WriteLine(email);
+
+    }
+
+}
+
---
+++ Method/one_method_all/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// create a object of class 
+// Object name -emp
+Employee emp = new Employee ();
+
+
+// object help us to access - everything in class 
+
+emp.name = "bhola"; // you accessed and called variable here
+emp.email = "bhola@gmail.com";
+emp.mobile = 7605865747;
+
+emp.Display();// you accessed and called method here
+
+
