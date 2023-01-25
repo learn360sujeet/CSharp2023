aa88ef36bc35eb7fa4ae7686b9673149348c6886
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-01-24 20:10:18 -0600

    constructor

---
+++ Method/nine_constructore/EmployeeConstructor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nine_constructore
+{
+    // 1. What is constrcutor 
+    // 2. What is condtion to create a object when you have constrcutor with paramter //new EmployeeConstructor(10, 123456789);
+    // 3. What is default constrcutor
+    // 4. 
+    internal class EmployeeConstructor
+    {
+
+        private int age;
+
+
+        public EmployeeConstructor() // whenever you create a object of this class then pass the parameter
+        {
+          
+
+
+        }
+
+        // contrcutor is special method which has same name as class name 
+        // contrcutor has same name as class name
+        // contrcutor does not have return type
+        public EmployeeConstructor(int _age) // whenever you create a object of this class then pass the parameter
+        {
+            age = _age;
+
+
+        }
+
+        public EmployeeConstructor(int _age , int mobile) // whenever you create a object of this class then pass the parameter
+        {
+            age = _age;
+
+
+        }
+        public void display()
+        {
+            Console.WriteLine("Age-" + age);
+        }
+    }
+}
---
+++ Method/nine_constructore/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using nine_constructore;
+
+EmployeeConstructor employeeConstructor = new EmployeeConstructor();
+
+EmployeeConstructor employeeConstructor = new EmployeeConstructor(10, 123456789);
+
+
+employeeConstructor.display();
