df6e80e23b257ea7e3ffbbd8a03f4eb232eaaea2
learn360sujeet <121271779+learn360sujeet@users.noreply.github.com>
2023-01-26 20:32:18 -0600

    code

---
+++ Interface/One_interface_simple/Icar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_interface_simple
+{
+    internal interface Icar
+    {
+        // method defination
+        void wheel_1();
+        void wheel_2();
+        void wheel_3();
+        void wheel_4();
+
+        void wheel_backup();
+    }
+}
---
+++ Interface/One_interface_simple/car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_interface_simple
+{
+    internal class car : Icar // implement 
+    {
+        public void wheel_1()
+        {
+            Console.Write("wheel_1 details and implementation");
+          
+        }
+
+        public void wheel_2()
+        {
+            Console.Write("wheel_3 details and implementation");
+        }
+
+        public void wheel_3()
+        {
+            Console.Write("wheel_3 details and implementation");
+        }
+
+        public void wheel_4()
+        {
+            Console.Write("wheel_4 details and implementation");
+        }
+        public void wheel_backup()
+        {
+            Console.Write("wheel_4 details and implementation");
+        }
+
+    }
+}
