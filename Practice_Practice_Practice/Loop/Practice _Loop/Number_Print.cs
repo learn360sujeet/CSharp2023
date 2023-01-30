88c5d0fc8e4a85377d2af120db398d31a37f1e39
learn360Sujeet <learn360sujeet@gmail.com>
2023-01-30 07:58:36 +0530

    practice.

---
+++ Practice_Practice_Practice/Loop/Practice _Loop/Number_Print.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice__Loop
+{
+    
+    class Number_Print
+    {
+        /// <summary>
+        /// Print all numbers from 1 to 10 using a for loop
+        /// 
+        /// </summary>
+        public void DisplayNumber()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        /// Print every third number from 1 to 30 using a for loop
+        /// </summary>
+        public void Numbers()
+        {
+            int counter=0;
+            
+            for (int i=1; i<=30;i++)
+            {
+                counter++;
+
+               if(counter==3)
+                {
+                    counter = 0;
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        /// <summary>
+        /// Write a C# program that uses a
+        /// while loop to print the numbers from 1 to 10 in reverse order.
+        /// </summary>
+        public void B()
+        {
+            int num = 10;
+            while(num>=1)
+            {
+                
+                Console.WriteLine(num);
+                num--;
+
+            }
+
+        }
+    }
+}
---
+++ Practice_Practice_Practice/Loop/Practice _Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice__Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number_Print number_Print=new Number_Print();
+            // number_Print.DisplayNumber();
+
+            //number_Print.Numbers();
+            number_Print.B();
+
+            Console.ReadLine();
+        }
+    }
+}
